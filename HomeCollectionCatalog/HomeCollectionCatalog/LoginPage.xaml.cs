8490a9c7f31b6a5d94b8772dbaa0600fb5ffb22b
maxim945w786 <maks337fat@mail.ru>
2026-05-28 15:15:08 +0300

    Add files via upload

---
+++ HomeCollectionCatalog/HomeCollectionCatalog/LoginPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace HomeCollectionCatalog
+{
+    public partial class LoginPage : Window
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(
+            object sender,
+            RoutedEventArgs e)
+        {
+            MessageBox.Show(
+                "Вход в систему в разработке",
+                "Информация",
+                MessageBoxButton.OK,
+                MessageBoxImage.Information
+            );
+        }
+
+        private void RegisterLink_MouseLeftButtonDown(
+            object sender,
+            MouseButtonEventArgs e)
+        {
+            RegistrationPage page =
+                new RegistrationPage();
+
+            page.Show();
+            Close();
+        }
+    }
+}
---
+++ HomeCollectionCatalog/HomeCollectionCatalog/RegistrationPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+
+namespace HomeCollectionCatalog
+{
+    public partial class RegistrationPage : Window
+    {
+        public RegistrationPage()
+        {
+            InitializeComponent();
+        }
+
+        private void RegisterButton_Click(
+            object sender,
+            RoutedEventArgs e)
+        {
+            if (PasswordBox.Password !=
+                ConfirmPasswordBox.Password)
+            {
+                MessageBox.Show(
+                    "Пароли не совпадают",
+                    "Ошибка регистрации",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error
+                );
+
+                return;
+            }
+
+            MessageBox.Show(
+                "Регистрация в разработке",
+                "Информация",
+                MessageBoxButton.OK,
+                MessageBoxImage.Information
+            );
+        }
+    }
+}
